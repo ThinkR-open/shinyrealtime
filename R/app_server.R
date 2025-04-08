70f63c49adaf671bed824eef7dc2ea9fde621e0c
Arthur Bréant <arthur@thinkr.fr>
2025-04-08 13:12:56 +0200

    fix: close session before to restart

--- R/app_server.R
+++ R/app_server.R
@@ -104,9 +104,21 @@ app_server <- function(input, output, session) {
     }
   })
 
+  observeEvent(input$user_inactive, {
+    cat_where(whereami())
+
+    showModal(modalDialog(
+      title = "You are inactive",
+      "You've been inactive for a while. Please refresh the page to continue.",
+      easyClose = FALSE,
+      footer = NULL
+    ))
+    session$close()
+  })
 
   session$onSessionEnded(function() {
     cat_where(whereami())
+    isolate(rv$pouet)
 
     if (getOption("golem.app.prod")) {
       body <- jsonlite::toJSON(
@@ -135,47 +147,6 @@ app_server <- function(input, output, session) {
         is_first_visit = c(FALSE, FALSE, FALSE)
       )
     }
-  })
-
-  observeEvent(input$user_inactive, {
-    cat_where(whereami())
-
-    showModal(modalDialog(
-      title = "You are inactive",
-      "You've been inactive for a while. Please refresh the page to continue.",
-      easyClose = FALSE,
-      footer = NULL
-    ))
-
-    if (getOption("golem.app.prod")) {
-
-      body <- jsonlite::toJSON(
-        list(
-          is_connected = FALSE
-        ),
-        auto_unbox = TRUE
-      )
-
-      response <- httr::PATCH(
-        url = paste0(rv$supabase_url, "/rest/v1/users?", "name=eq.", rv$connected_user),
-        httr::add_headers(
-          "apikey" = rv$supabase_key,
-          "Content-Type" = "application/json",
-          "Prefer" = "return=representation"
-        ),
-        body = body,
-        encode = "json"
-      )
-
-    } else {
-      rv$users <- data.frame(
-        id = c(1, 2, 3),
-        name = c("user1", "user2", "user3"),
-        is_connected = c(FALSE, FALSE, FALSE),
-        is_first_visit = c(FALSE, FALSE, FALSE)
-      )
-    }
-
   })
 
   mod_navbar_server(
